4f2ac84dada59e7d105089d2a85a85933b51c826
Kunjesh Kumar <kunjesh.kumar@laureate.net>
2020-09-14 03:25:53 +0530

    added personalised styles

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,45 +1,136 @@
-import React, { Component } from 'react';
+import React from "react";
+import PropTypes from "prop-types";
+import SwipeableViews from "react-swipeable-views";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+import { white } from "color-name";
 
-class Portfolio extends Component {
-  render() {
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
 
-    if(this.props.data){
-      var projects = this.props.data.projects.map(function(projects){
-        var projectImage = 'images/portfolio/'+projects.image;
-        return <div key={projects.title} className="columns portfolio-item">
-           <div className="item-wrap">
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired
+};
+
+function a11yProps(index) {
+  return {
+    id: `full-width-tab-${index}`,
+    "aria-controls": `full-width-tabpanel-${index}`
+  };
+}
+
+// const useStyles = makeStyles(theme => ({
+//   root: {
+//     backgroundColor: white,
+//     width: 1000
+//   }
+// }));
+
+export default function Portfolio({ data }) {
+  // const classes = useStyles();
+  const theme = useTheme();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleChangeIndex = index => {
+    setValue(index);
+  };
+
+  if (data) {
+    var projects = data.projects.map(function(projects) {
+      var projectImage = "images/portfolio/" + projects.image;
+      return (
+        <div key={projects.title} className="columns portfolio-item">
+          <div className="item-wrap">
             <a href={projects.url} title={projects.title}>
-               <img alt={projects.title} src={projectImage} />
-               <div className="overlay">
-                  <div className="portfolio-item-meta">
-                 <h5>{projects.title}</h5>
-                     <p>{projects.category}</p>
-                  </div>
+              <img alt={projects.title} src={projectImage} />
+              <div className="overlay">
+                <div className="portfolio-item-meta">
+                  <h5>{projects.title}</h5>
+                  <p>{projects.category}</p>
                 </div>
-              <div className="link-icon"><i className="fa fa-link"></i></div>
+              </div>
+              <div className="link-icon">
+                <i className="fa fa-link"></i>
+              </div>
             </a>
           </div>
         </div>
-      })
-    }
-
-    return (
-      <section id="portfolio">
-
-      <div className="row">
-
-         <div className="twelve columns collapsed">
-
-            <h1>Check Out Some of My Works.</h1>
-
-            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-                {projects}
-            </div>
-          </div>
-      </div>
-   </section>
-    );
+      );
+    });
   }
-}
 
-export default Portfolio;
+  return (
+    <section id="portfolio">
+      <div className="row">
+        <div className="twelve columns collapsed">
+          <h1>Check Out Some of My Works.</h1>
+
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            indicatorColor="primary"
+            textColor="primary"
+            variant="fullWidth"
+            aria-label="full width tabs example"
+          >
+            <Tab label="ALL" {...a11yProps(0)} />
+            <Tab label="React JS/ Node JS" {...a11yProps(1)} />
+            <Tab label="ML/AI" {...a11yProps(2)} />
+          </Tabs>
+
+          <SwipeableViews
+            axis={theme.direction === "rtl" ? "x-reverse" : "x"}
+            index={value}
+            onChangeIndex={handleChangeIndex}
+          >
+            <TabPanel value={value} index={0} dir={theme.direction}>
+              <div className="row">
+                <div className="twelve columns collapsed">
+                  <div
+                    id="portfolio-wrapper"
+                    className="bgrid-halves s-bgrid-thirds cf"
+                  >
+                    {projects}
+                  </div>
+                </div>
+              </div>
+            </TabPanel>
+            <TabPanel value={value} index={1} dir={theme.direction}>
+              Item Two
+            </TabPanel>
+            <TabPanel value={value} index={2} dir={theme.direction}>
+              Item Three
+            </TabPanel>
+          </SwipeableViews>
+        </div>
+      </div>
+    </section>
+  );
+}
